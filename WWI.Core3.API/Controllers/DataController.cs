953437bada9f965a5a82be527b4734f997c2470e
Mustafizur Rohman <mustafizur.rohman88@gmail.com>
2020-05-13 21:48:17 +0200

    Added Methods to DataController

--- WWI.Core3.API/Controllers/DataController.cs
+++ WWI.Core3.API/Controllers/DataController.cs
@@ -98,6 +98,70 @@ namespace WWI.Core3.API.Controllers
             return Ok(doctors);
         }
 
+        /// <summary>
+        /// Gets the hospitals.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("hospitals")]
+        public async Task<IActionResult> GetHospitals()
+        {
+            var hospitals = await DbContext.Hospitals
+                .ToListAsync();
+
+            return Ok(hospitals);
+        }
+
+        /// <summary>
+        /// Gets the hospital by identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        [HttpGet("hospitals/{id}")]
+        public async Task<IActionResult> GetHospitalById(int id)
+        {
+            var hospital = await DbContext.Hospitals
+                .FindAsync(id);
+
+            return Ok(hospital);
+        }
+
+        /// <summary>
+        /// Gets the doctors for hospital by identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        [HttpGet("hospitals/{id}/doctors")]
+        public async Task<IActionResult> GetDoctorsForHospitalById(int id)
+        {
+            var doctorsInHospital = (await DbContext.Hospitals
+                .Include(h => h.Doctors)
+                .ThenInclude(h => h.Doctor)
+                .ThenInclude(doc => doc.Speciality)
+                .Where(h => h.HospitalID == id)
+                .SelectMany(h => h.Doctors)
+                .Select(hd => hd.Doctor)
+                .Select(d => new
+                {
+                    d.Firstname,
+                    d.Middlename,
+                    d.Lastname,
+                    SpecialityName = d.Speciality.Name
+                })
+                .ToListAsync())
+                .Select(doc => new
+                {
+                    Name = Regex.Replace(doc.Firstname + " " + doc.Middlename + " " + doc.Lastname, @"\s+", " "),
+                    Speciality = doc.SpecialityName
+                })
+                .OrderBy(doc => doc.Speciality)
+                .ThenBy(doc => doc.Name)
+                .ToList();
+
+            return Ok(doctorsInHospital);
+        }
+
+
+
 
     }
 }
