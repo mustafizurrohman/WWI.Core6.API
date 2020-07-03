fcd47df5081b6c9cf6a2bf09669dd704107e9e08
Mustafizur Rohman <mustafizur.rohman88@gmail.com>
2020-07-03 06:57:42 +0200

    Added UnitTests

---
+++ WWI.Core3.Core.Tests/ExtensionMethods/EnumerableExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using WWI.Core3.Core.ExtensionMethods;
+using WWI.Core3.Core.Helpers;
+using Xunit;
+
+namespace WWI.Core3.Core.Tests.ExtensionMethods
+{
+
+    /// <summary>
+    /// Class EnumerableExtensionsTests.
+    /// </summary>
+    public class EnumerableExtensionsTests
+    {
+
+        /// <summary>
+        /// Verifies the that shuffe does not delete elements.
+        /// </summary>
+        /// <param name="numberOfElements">The number of elements.</param>
+        [Theory]
+        [InlineData(100)]
+        public void VerifyThatShuffeDoesNotDeleteElements(int numberOfElements)
+        {
+            var originalList = Enumerable.Range(0, numberOfElements)
+                .Select(num => IntHelpers.GetRandomNumber(1, numberOfElements))
+                .ToList();
+
+            var shuffledList = originalList.Shuffle().ToList();
+
+            var diff = originalList.Except(shuffledList).ToList();
+
+            Assert.True(diff.Count == 0);
+            Assert.True(diff.IsEmpty());
+
+            Assert.True(originalList.Count == shuffledList.Count);
+        }
+
+    }
+
+}
---
+++ WWI.Core3.Core.Tests/ExtensionMethods/StringExtensionsTests.cs
@@ -0,0 +1,16 @@
+﻿using WWI.Core3.Core.ExtensionMethods;
+using Xunit;
+
+namespace WWI.Core3.Core.Tests.ExtensionMethods
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData("Aa1*df2d2")]
+        public void TestValidPasswords(string password)
+        {
+            Assert.True(password.IsValidPassword());
+        }
+
+    }
+}
