2fc9975f74b2fca0db2a94a1963b82b70da9758d
Mustafizur Rohman <mustafizur.rohman88@gmail.com>
2021-12-17 16:04:54 +0100

    Added Playground Controller

---
+++ WWI.Core3.API/Controllers/PlaygroundController.cs
@@ -0,0 +1,128 @@
+﻿using Ardalis.GuardClauses;
+using Bogus;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using WWI.Core3.API.Controllers.Base;
+using WWI.Core3.Services.Interfaces;
+using WWI.Core3.Services.ServiceCollection;
+
+namespace WWI.Core3.API.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class PlaygroundController : BaseAPIController
+    {
+        private IFakeDataGeneratorService FakeDataGeneratorService { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fakeDataGeneratorService"></param>
+        /// <param name="applicationServices"></param>
+        public PlaygroundController(IFakeDataGeneratorService fakeDataGeneratorService, IApplicationServices applicationServices)
+            : base(applicationServices)
+        {
+            FakeDataGeneratorService = Guard.Against.Null(fakeDataGeneratorService, nameof(fakeDataGeneratorService)); 
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        [HttpGet("bogusNames")]
+        public IActionResult GetNames(int num)
+        {
+            var doctors = FakeDataGeneratorService.GenerateFakeDoctors(num);
+            return Ok(doctors);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        [HttpGet("MaxBy")]
+        public IActionResult TestMaxBy(int num)
+        {
+            num = Guard.Against.NegativeOrZero(num, nameof(num));
+            
+            var faker = new Faker();
+            
+            var maxByValue = Enumerable.Range(0, num)
+                .Select(x => new
+                {
+                    Name = faker.Name.FirstName() + " " + faker.Name.LastName(),
+                    Age = faker.Random.Number(50, 70)
+                })
+                .MaxBy(x => x.Age);
+
+            return Ok(maxByValue);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        [HttpGet("LINQChunk")]
+        public IActionResult LinqChunkTest(int num)
+        {
+            var doctors = FakeDataGeneratorService.GenerateFakeDoctors(num);
+
+            var chunked = doctors
+                .Select(p => p.FullName)
+                .Chunk((int)Math.Sqrt(num));
+
+            return Ok(chunked);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        [HttpGet("LINQOverloads")]
+        public IActionResult OverLoadTest(int num)
+        {
+            num = Guard.Against.NegativeOrZero(num, nameof(num));
+
+            var randomNumbers = Enumerable.Range(1, num)
+                .Select(x => new Faker().Random.Number(10, 100))
+                .ToList();
+
+            var numberslessThan10 = randomNumbers.FirstOrDefault(x => x < 10, -1);
+
+            return Ok(numberslessThan10);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("ConstantInterpolatedStrings")]
+        public IActionResult ConstantInterpolatedStrings()
+        {
+            const string languageReleasePrefix = "C# 10";
+            const string languageRelease = $"{ languageReleasePrefix } to be released in November 2021.";
+
+            return Ok(languageRelease);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("lambda")]
+        public IActionResult LambdaImprovements()
+        {
+            var helloWorld = () => "Hello World";
+
+            return Ok();
+        }
+
+    }
+}
